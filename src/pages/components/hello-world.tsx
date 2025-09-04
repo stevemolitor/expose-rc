67ced920fdfec41b6511313324e129b2c9e50457
Steve Molitor <steve.molitor@zapier.com>
2025-09-04 16:14:53 -0500

    Revert to Pages Router for testing
    - Remove App Router structure
    - Restore Pages Router with _app.tsx configuration
    - Keep middleware for CORS support
    - Testing showed both router types work with remote components

---
+++ src/pages/components/hello-world.tsx
@@ -0,0 +1,8 @@
+export default function HelloWorldPage() {
+  return (
+    <div>
+      <h1>Hello from a Remote Component (Pages Router)</h1>
+      <p>This component is served from expose-rc using Pages Router!</p>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-export default function HomePage() {
+export default function Home() {
   return (
     <div>
       <h1>Expose RC - Remote Components Demo</h1>
       <p>Available remote components:</p>
       <ul>
         <li>
-          <a href="/components/hello-world">App Router: /components/hello-world</a>
+          <a href="/components/hello-world">Pages Router: /components/hello-world</a>
         </li>
       </ul>
     </div>
